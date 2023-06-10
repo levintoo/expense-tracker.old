4cedd1bf319f34b3794c521d5e0821223f3f77d3
levintoo <tooklevin@gmail.com>
2023-06-10 16:19:10 +0300

    delete items using Link component

--- resources/js/Components/CashFlowTable.vue
+++ resources/js/Components/CashFlowTable.vue
@@ -1,24 +1,16 @@
 <script setup>
 import SecondaryButton from "@/Components/SecondaryButton.vue";
-import DangerButton from "@/Components/DangerButton.vue";
 import Checkbox from "@/Components/Checkbox.vue";
 import PrimaryLink from "@/Components/PrimaryLink.vue";
-import { useForm } from '@inertiajs/inertia-vue3';
 import Pagination from "@/Components/Pagination.vue";
+import TextInput from "@/Components/TextInput.vue";
+import DangerLink from "@/Components/DangerLink.vue";
 
 const props = defineProps({
-    data: Array,
+    data: Object,
     category: String,
-    postroute: String,
+    postRoute: String,
 });
-
-const form = useForm();
-
-function destroy(id) {
-    if (confirm("Are you sure you want to Delete")) {
-        form.delete(route(props.postroute+'.delete', id));
-    }
-}
 
 </script>
 
@@ -26,7 +18,7 @@ function destroy(id) {
 
     <div class="flex items-center justify-between py-4 bg-white dark:bg-gray-800">
 
-        <PrimaryLink :href="route(postroute+'.create')">
+        <PrimaryLink :href="route(postRoute+'.create')">
             Add {{ category }}
         </PrimaryLink>
 
@@ -35,7 +27,7 @@ function destroy(id) {
             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
             </div>
-            <input type="text" id="table-search-users" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" :placeholder="`Search for ${category}`">
+            <TextInput type="text" id="table-search-users" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" :placeholder="`Search for ${category}`"></TextInput>
         </div>
     </div>
 
@@ -65,7 +57,7 @@ function destroy(id) {
             </th>
         </tr>
         </thead>
-        <tbody>
+        <tbody v-if="data.data.length > 0">
         <tr v-for="datum in data.data"
             class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
             <td class="w-4 p-4">
@@ -85,18 +77,22 @@ function destroy(id) {
                 {{ datum.description }}
             </td>
             <td class="px-6 py-4">
-                <div class="flex items-center">
-                    <div class="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div>
                     {{ datum.category }}
-                </div>
             </td>
             <td class="px-6 py-4">
                 <SecondaryButton class="m-1">View({{ datum.id }})</SecondaryButton>
-                <PrimaryLink class="m-1" :href="route(postroute+'.edit', datum.id)">Edit({{ datum.id }})</PrimaryLink>
-                <DangerButton class="m-1" @click="destroy(datum.id)">Delete me({{ datum.id }})</DangerButton>
+                <PrimaryLink class="m-1" :href="route(postRoute+'.edit', datum.id)">Edit({{ datum.id }})</PrimaryLink>
+                <DangerLink as="button" method="delete" :href="route(props.postRoute+'.delete', datum.id)" class="m-1">Delete me({{ datum.id }})</DangerLink>
             </td>
         </tr>
         </tbody>
-        <Pagination class="mt-6" :links="data.links" />
+        <tbody v-else>
+        <tr>
+        <td class="px-6 py-4 text-center text-gray-400" colspan="6">
+                There is nothing to show here
+        </td>
+        </tr>
+        </tbody>
     </table>
+        <Pagination :links="data.links" />
 </template>
